8992a04ba8c4fe20933022dda19e04246642d8a8
agustinsilva447 <agustinsilva447@gmail.com>
2023-09-25 16:27:38 +0200

    vhdl qmarl

---
+++ tb_agent.sv
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+
+module tb_agent();
+    
+    bit clk, rst, v, d;
+    logic [15:0] r;
+    logic [8:0] a;
+    
+    agent a1(clk,rst,v,r,a,d);
+    always #10 clk <= ~clk;
+    
+    initial begin
+        clk <= 1;
+        rst <= 1;
+        v <= 0;
+        r <= 0;
+        repeat(5) @(posedge clk);
+        rst <= 0;
+        
+        for(int i=0; i<=100; i++) 
+        begin
+//            if (i%10 == 0) begin
+//                rst <= 1;
+//                repeat(2) @(posedge clk);
+//                rst <= 0;                
+//            end   
+            repeat(1) @(posedge clk);
+            r <= $urandom_range(0,32767);        
+            v <= 1;
+            repeat(1) @(posedge clk);
+            v <= 0;
+            @(posedge d);      
+        end        
+        #1000
+        $finish();
+    end    
+
+endmodule
